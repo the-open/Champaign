fbbaff4a5bac2387dab451a47b23d44732ecffe9
Vincent Martinez <eykosioux@gmail.com>
2018-02-28 15:16:33 +0000

    Upload assets to nothing :fire:

--- .circleci/upload_assets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eu -o pipefail
-STATIC_BUCKET=$1
-echo 'Uploading assets and packs to S3...'
-aws s3 sync public/assets s3://$STATIC_BUCKET/assets/
-aws s3 sync public/packs/ s3://$STATIC_BUCKET/packs/
